658e6f2e053d6213927eef978c6bd07cf902bc1e
yoshipc <yoooo@yoshipc.net>
2017-02-09 00:26:15 +0900

    ボタンの指定の変更
    おまけです。

--- quiz.js
+++ quiz.js
@@ -3,9 +3,9 @@ function button1(){
 }
 
 function button2(){
-  document.getElementById('button1').textContent="Yeah,Wrong!";
+  document.getElementById('button2').textContent="Yeah,Wrong!";
 }
 
 function button3(){
-  document.getElementById('button1').textContent="Yeah,Wrong!";
+  document.getElementById('button3').textContent="Yeah,Wrong!";
 }
