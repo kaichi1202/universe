ccc7690d1fda480d173fdd9f4c8cb914dd27bf79
yoshipc <yosshisan_pc@yahoo.co.jp>
2017-02-09 00:17:21 +0900

    クイズ作成の準備
    クイズ作成の準備をしました。ボタンのCSSも書き換えました。どうやってクイズ作ろうか。

--- quiz.js
+++ quiz.js
@@ -1,8 +1,11 @@
 function button1(){
+  document.getElementById('button1').textContent="Yeah,Right!";
 }
 
 function button2(){
+  document.getElementById('button1').textContent="Yeah,Wrong!";
 }
 
 function button3(){
+  document.getElementById('button1').textContent="Yeah,Wrong!";
 }
